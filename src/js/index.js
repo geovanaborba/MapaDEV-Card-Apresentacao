ad5647be31fd4a1b29e4350a6339c2bf6fdcc077
Geovana Borba <geborba19@hotmail.com>
2022-10-31 15:11:22 -0300

    codigo js refatorado

--- src/js/index.js
+++ src/js/index.js
@@ -13,30 +13,38 @@ abas.forEach(aba => {
             return; //evita que rode código que não precisa
         }
 
-        // - passo 3: quando usuário clicar, desmarcar a aba selecionada
-        const abaSelecionada = document.querySelector(".aba.selecionado");
-        abaSelecionada.classList.remove("selecionado");
+        selecionarAba(aba);
 
-        // - passo 4: marcar a aba clicada como selecionado
-        aba.classList.add("selecionado");
-
-        // - passo 5: esconder o conteúdo anterior
-        const informacaoSelecionada = document.querySelector(".informacao.selecionado");
-
-        informacaoSelecionada.classList.remove("selecionado");
-
-        // - passo 6: mostrar o conteúdo da aba selecionada
-        const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
-        //const idDoElementoDeInformacoesDaAba = "informacao-" +aba.id; também é uma opção para usar acima para concatenação
-
-        const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
-        informacaoASerMostrada.classList.add("selecionado");
+        mostrarInformacoesDaAba(aba);
 
     });
 });
 
+function selecionarAba(aba) {
 
+    // - passo 3: quando usuário clicar, desmarcar a aba selecionada
+    const abaSelecionada = document.querySelector(".aba.selecionado");
+    abaSelecionada.classList.remove("selecionado");
 
+    // - passo 4: marcar a aba clicada como selecionado
+    aba.classList.add("selecionado");
+}
+
+function mostrarInformacoesDaAba(aba) {
+
+    // - passo 5: esconder o conteúdo anterior
+    const informacaoSelecionada = document.querySelector(".informacao.selecionado");
+
+    informacaoSelecionada.classList.remove("selecionado");
+
+    // - passo 6: mostrar o conteúdo da aba selecionada
+    const idDoElementoDeInformacoesDaAba = `informacao-${aba.id}`;
+    //const idDoElementoDeInformacoesDaAba = "informacao-" +aba.id; também é uma opção para usar acima para concatenação
+
+    const informacaoASerMostrada = document.getElementById(idDoElementoDeInformacoesDaAba);
+    informacaoASerMostrada.classList.add("selecionado");
+
+}
 
 
 
